7fcfa76b34bcad49e651161a263fbd8e0e826695
etsibareva <etsibareva.university@gmail.com>
2026-02-09 13:17:44 +0300

    Commit0: Init

--- test/tests.cpp
+++ test/tests.cpp
@@ -12,3 +12,22 @@ TEST(algtest, test2) {
   uint64_t result = factorial(2);
   EXPECT_EQ(2, result);
 }
+
+TEST(algtest, test3) {
+  uint64_t result = factorial(0);
+  EXPECT_EQ(1, result);
+}
+
+TEST(algtest, test4) {
+  result = factorial(16);
+  EXPECT_EQ(20922789888000ULL, result);
+}
+
+TEST(algtest, test5) {
+  result = factorial(20);
+  EXPECT_EQ(2432902008176640000ULL, result);
+}
+
+TEST(algtest, test6) {
+  EXPECT_THROW(factorial(21), std::overflow_error);
+}
