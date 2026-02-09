c3be3559351fcc3e4dd610e65cf8e0565ecc5e86
KotelnikovaA <rabatenok@gmail.com>
2026-02-09 11:33:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,15 @@
 #include <cstdint>
 
 uint64_t factorial(uint8_t n) {
-  return 1;
+    if (n == 0) {
+        return 1;
+    }
+
+    uint64_t result = 1;
+    for (uint8_t i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+
+    return result;
 }
