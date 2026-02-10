df837844e963105137f8165d5ba5e0910aabdf47
Alexey Posternak <lockharn@student.21-school.ru>
2026-02-10 19:25:05 +0300

    first

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,14 @@
 // Copyright 2026 NNTU-CS
 #include <cstdint>
+#include "alg.h"
 
 uint64_t factorial(uint8_t n) {
-  return 1;
+  uint64_t res = 1;
+  if (n < 2) {
+    return res;
+  }
+  for (uint8_t i = 2; i <= n; i++){
+    res *= i;
+  }
+  return res;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -12,3 +12,8 @@ TEST(algtest, test2) {
   uint64_t result = factorial(2);
   EXPECT_EQ(2, result);
 }
+
+TEST(algtest, test3) {
+  uint64_t result = factorial(10);
+  EXPECT_EQ(3628800, result);
+}
