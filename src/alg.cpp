15bbe4aefd718d5707e0cb5af7ddf7800edae6d5
cedricir <cedricir@student.21-school.ru>
2026-02-14 11:41:03 +0300

    fix4

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2026 NNTU-CS
 #include <cstdint>
+#include "alg.h"
 
 uint64_t factorial(uint8_t n) {
   if (n > 20) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2026 NNTU-CS
 #include <iostream>
 #include <cstdint>
-#include "../include/alg.h"
+#include "alg.h"
 
 int main() {
     uint64_t result = factorial(2);
