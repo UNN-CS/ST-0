66f1d351a4a104c8b7a66fc2c9f85a740404d14e
Anuta200 <nyutik05@gmail.com>
2026-02-15 20:52:14 +0300

    Test

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,22 @@
 // Copyright 2026 NNTU-CS
 #include <cstdint>
+#include <limits>
+#include <stdexcept>
 
 uint64_t factorial(uint8_t n) {
-  return 1;
+  if (n == 0) {
+    return 1;
+  }
+
+  uint64_t result = 1;
+
+  for (uint64_t i = 1; i <= n; ++i) {
+    if (result > std::numeric_limits<uint64_t>::max() / i) {
+      throw std::overflow_error("factorial overflow");
+    }
+
+    result *= i;
+  }
+
+  return result;
 }
