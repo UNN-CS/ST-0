e82bf98f27429875339e7607821084c153e2e0cf
Serebro1 <OtcheskovSemen@yandex.ru>
2026-02-15 23:00:49 +0300

    add factorial tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,12 +3,35 @@
 #include "gtest/gtest.h"
 #include "alg.h"
 
-TEST(algtest, test1) {
+TEST(algtest, EdgeCaseFactorialZero) {
+  uint64_t result = factorial(0);
+  EXPECT_EQ(1, result);
+}
+
+TEST(algtest, EdgeCaseFactorialOne) {
   uint64_t result = factorial(1);
   EXPECT_EQ(1, result);
 }
 
-TEST(algtest, test2) {
+TEST(algtest, FactorialTwo) {
   uint64_t result = factorial(2);
   EXPECT_EQ(2, result);
+}
+
+TEST(algtest, FactorialTen) {
+  uint64_t result = factorial(10);
+  EXPECT_EQ(3628800, result);
+}
+
+TEST(algtest, FactorialMaxValidValue) {
+  uint64_t result = factorial(20);
+  EXPECT_EQ(2432902008176640000ULL, result);
+}
+
+TEST(algtest, FactorialExceedMaxValidValue) {
+  EXPECT_THROW(factorial(21), std::overflow_error);
+}
+
+TEST(algtest, FactorialNegativeInt) {
+  EXPECT_THROW(factorial(-1), std::overflow_error);
 }
