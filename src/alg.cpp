05c63849dbbb70e60d8dc13ff94e96febd8b2ee7
SafronovMaks1m <safranov01@list.ru>
2026-02-15 14:56:37 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,14 @@
 // Copyright 2026 NNTU-CS
 #include <cstdint>
+#include <stdexcept>
 
 uint64_t factorial(uint8_t n) {
-  return 1;
+	if (n > 20) {
+		throw std::out_of_range("n must be <= 20 for uint64_t factorial");
+	}
+	uint64_t res = 1;
+	for (uint8_t i = 1; i <= n; i++) {
+		res *= i;
+	}
+	return res;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -12,3 +12,22 @@ TEST(algtest, test2) {
   uint64_t result = factorial(2);
   EXPECT_EQ(2, result);
 }
+
+TEST(algtest, test3) {
+	uint64_t result = factorial(0);
+	EXPECT_EQ(1, result);
+}
+
+TEST(algtest, test4) {
+	uint64_t result = factorial(5);
+	EXPECT_EQ(120, result);
+}
+
+TEST(algtest, test5) {
+	uint64_t result = factorial(12);
+	EXPECT_EQ(479001600, result);
+}
+
+TEST(algtest, test6) {
+	EXPECT_THROW(factorial(21), std::out_of_range);
+}
