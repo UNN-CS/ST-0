10de5adf49957754f201f3160af20db762ecebd2
Alina <red.alinka.malinka@gmail.com>
2026-02-16 10:51:41 +0300

    done4

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,17 @@
 // Copyright 2026 NNTU-CS
 #include <cstdint>
+#include <stdexcept>
 
 uint64_t factorial(uint8_t n) {
-	if (n == 0) {
-		return 1;
-	}
-	else if (n > 20) {
-		throw -1;
-	}
-	else {
-		uint64_t ans = 1;
-		for (uint64_t i = 2; i <= n; i++) {
-			ans *= i;
-		}
-		return ans;
-	}
+    if (n == 0) {
+        return 1;
+    } else if (n > 20) {
+        throw std::invalid_argument("Factorial argument too large");
+    } else {
+        uint64_t ans = 1;
+        for (uint64_t i = 2; i <= n; ++i) {
+            ans *= i;
+        }
+        return ans;
+    }
 }
-
-
