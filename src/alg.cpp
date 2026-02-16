835d259c701e95ecb26fa678702e678a413225ce
Alina <red.alinka.malinka@gmail.com>
2026-02-16 10:37:15 +0300

    done

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,20 @@
 #include <cstdint>
 
 uint64_t factorial(uint8_t n) {
-  return 1;
+	if (n < 0) {
+		throw -1;
+	}
+	else if (n == 0) {
+		return 1;
+	}
+	else if (n > 20) {
+		throw -1;
+	}
+	else {
+		uint64_t ans = 1;
+		for (uint64_t i = 2; i <= n; i++) {
+			ans *= i;
+		}
+		return ans;
+	}
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -12,3 +12,29 @@ TEST(algtest, test2) {
   uint64_t result = factorial(2);
   EXPECT_EQ(2, result);
 }
+
+TEST(algtest, test3) {
+  uint64_t result = factorial(3);
+  EXPECT_EQ(6, result);
+}
+
+TEST(algtest, test4) {
+  EXPECT_ANY_THROW(factorial(-1));
+}
+
+TEST(algtest, test5) {
+  EXPECT_ANY_THROW(factorial(-10));
+}
+
+TEST(algtest, test6) {
+  EXPECT_NO_THROW(factorial(5));
+}
+
+TEST(algtest, test7) {
+  EXPECT_ANY_THROW(factorial(30));
+}
+
+TEST(algtest, test8) {
+  uint64_t result = factorial(0);
+  EXPECT_EQ(1, result);
+}
